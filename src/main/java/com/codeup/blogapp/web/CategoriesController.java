49783d79f7b4fcfcc96957a5e677fa8d12fa86a1
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-08-19 16:57:04 -0500

    adds more to categories

--- src/main/java/com/codeup/blogapp/web/CategoriesController.java
+++ src/main/java/com/codeup/blogapp/web/CategoriesController.java
@@ -1,4 +1,14 @@
 package com.codeup.blogapp.web;
 
+
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 public class CategoriesController {
+
+    private Object Category;
+
+    private Object getPostsByCategory(@RequestParam String categoryName) {
+        return Category;
+    }
 }
