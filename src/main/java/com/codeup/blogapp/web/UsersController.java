98a497fb0dc4f7b0d410b3c094b9103bc55b0b0c
tiffannyhadden <tiffanny.hadden@gmail.com>
2021-08-19 11:25:49 -0500

    adds more to users controller

--- src/main/java/com/codeup/blogapp/web/UsersController.java
+++ src/main/java/com/codeup/blogapp/web/UsersController.java
@@ -14,37 +14,63 @@ public class UsersController {
     private List<User> getUsers() {
         return new ArrayList<>() {
             {
-                add(new User(1L, "amadoIII", "amadoiii@gmail.com","amado@III"));
-                add(new User(1L, "richardL", "richardL@gmail.com","RichardL"));
-                add(new User(1L, "PrachiP", "prachiP@gmail.com","PrachiP"));
+                add(new User(1L, "amadoIII", "amadoiii@gmail.com", "amado@III"));
+                add(new User(1L, "richardL", "richardL@gmail.com", "RichardL"));
+                add(new User(1L, "PrachiP", "prachiP@gmail.com", "PrachiP"));
             }
         };
     }
 
 
-
     @GetMapping("/{id}")
-    private User getUserById(@PathVariable Long id){
+    private User getUserById(@PathVariable Long id) {
         // /api/posts/1
-        if(id == 1) {
+        if (id == 1) {
             return new User();
-        }else{
+        } else {
             return null;
         }
 
     }
+
     @PostMapping
-    private void createUser(@RequestBody User newUser){
+    private void createUser(@RequestBody User newUser) {
         System.out.println(newUser.getUsername());
     }
 
     @PutMapping("{id}")
-    private void updateUser(PathVariable id, @RequestBody User user){
+    private void updateUser(PathVariable id, @RequestBody User user) {
         System.out.println(id);
     }
 
     @DeleteMapping("{id}")
-    private void deleteUser(@PathVariable Long id){
+    private void deleteUser(@PathVariable Long id) {
         System.out.println(id);
     }
+
+    @GetMapping("/findByEmail")
+    private User findByEmail(@RequestParam String email) {
+
+        if (email.equals("tiffanny@codeup.com")) {
+            return new User("Tiffanny");
+        } else if (email.equals("prachi@codeup.com")) {
+            return new User("prachi");
+        } else
+            return null;
+    }
+
+    @GetMapping("/findByUsername")
+    private User findByUsername(@RequestParam String username) {
+
+        if (username.equals("tiffanny")) {
+            return new User("Tiffanny");
+        } else if (username.equals("prachi")) {
+            return new User("Prachi");
+        } else
+            return null;
+    }
+
+
+
 }
+
